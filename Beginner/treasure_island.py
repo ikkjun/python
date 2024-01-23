3a95611b2ae33e65b13d61364f0b51244f063f2c
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-01-23 20:36:51 +0900

    study python

--- Beginner/treasure_island.py
+++ Beginner/treasure_island.py
@@ -27,16 +27,16 @@ print('''
 print("Welcome to Tresure Island.")
 print("Your mission is to find the treasure.")
 
-if input("left or right? ").lower() == "left" or "l":
-  if input("swim or wait? ").lower() == "wait":
-    door = input("Which door?").lower()
+if input("You're at a crossroad, where do you want to go? Type \"left\" or \"right\". ").lower() == "left" or "l":
+  if input("You've come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim across. ").lower() == "wait":
+    door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you want to choose? ").lower()
     if door == "red":
-      print(f"Burned by fire. \n Game Over")
+      print(f"Burned by fire.\nGame Over")
     elif door == "yellow":
       print("You Win!")
     elif door == "blue":
-      print(f"Eaten by beasts. \n GameOver")
+      print(f"Eaten by beasts.\nGameOver")
   else:
-    print(f"Attacked by trout.\n Game Over.")
+    print(f"Attacked by trout.\nGame Over.")
 else:
   print("Fall into a hole.\n Game Over.")
